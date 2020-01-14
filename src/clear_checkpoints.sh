efda8e3e60346dc51b485eb7b017e2a539e6316a
chauhanjatin10 <cs17btech11019@iith.ac.in>
2020-01-14 18:49:30 +0530

    reddit, enzymes added

--- src/clear_checkpoints.sh
+++ src/clear_checkpoints.sh
@@ -17,3 +17,21 @@ rm checkpoints/TRIANGLES/fine_tuned_test/gat_modules/*
 rm checkpoints/TRIANGLES/fine_tuned_test/classifier/*
 rm checkpoints/TRIANGLES/fine_tuned_test/gin_classifier/*
 rm checkpoints/TRIANGLES/fine_tuned_test/gin_model/*
+
+rm checkpoints/ENZYMES/main_model/gin_model/*
+rm checkpoints/ENZYMES/main_model/gin_classifier/*
+rm checkpoints/ENZYMES/main_model/gat_modules/*
+
+rm checkpoints/ENZYMES/fine_tuned_test/gat_modules/*
+rm checkpoints/ENZYMES/fine_tuned_test/classifier/*
+rm checkpoints/ENZYMES/fine_tuned_test/gin_classifier/*
+rm checkpoints/ENZYMES/fine_tuned_test/gin_model/*
+
+rm checkpoints/Reddit/main_model/gin_model/*
+rm checkpoints/Reddit/main_model/gin_classifier/*
+rm checkpoints/Reddit/main_model/gat_modules/*
+
+rm checkpoints/Reddit/fine_tuned_test/gat_modules/*
+rm checkpoints/Reddit/fine_tuned_test/classifier/*
+rm checkpoints/Reddit/fine_tuned_test/gin_classifier/*
+rm checkpoints/Reddit/fine_tuned_test/gin_model/*
--- src/prepare_checkpoints.sh
+++ src/prepare_checkpoints.sh
@@ -21,3 +21,26 @@ mkdir checkpoints/TRIANGLES/fine_tuned_test/gat_modules
 mkdir checkpoints/TRIANGLES/fine_tuned_test/classifier
 mkdir checkpoints/TRIANGLES/fine_tuned_test/gin_classifier
 mkdir checkpoints/TRIANGLES/fine_tuned_test/gin_model
+
+mkdir checkpoints/ENZYMES/main_model
+mkdir checkpoints/ENZYMES/main_model/gin_model
+mkdir checkpoints/ENZYMES/main_model/gin_classifier
+mkdir checkpoints/ENZYMES/main_model/gat_modules
+
+mkdir checkpoints/ENZYMES/fine_tuned_test
+mkdir checkpoints/ENZYMES/fine_tuned_test/gat_modules
+mkdir checkpoints/ENZYMES/fine_tuned_test/classifier
+mkdir checkpoints/ENZYMES/fine_tuned_test/gin_classifier
+mkdir checkpoints/ENZYMES/fine_tuned_test/gin_model
+
+mkdir checkpoints/Reddit/main_model
+mkdir checkpoints/Reddit/main_model/gin_model
+mkdir checkpoints/Reddit/main_model/gin_classifier
+mkdir checkpoints/Reddit/main_model/gat_modules
+
+mkdir checkpoints/Reddit/fine_tuned_test
+mkdir checkpoints/Reddit/fine_tuned_test/gat_modules
+mkdir checkpoints/Reddit/fine_tuned_test/classifier
+mkdir checkpoints/Reddit/fine_tuned_test/gin_classifier
+mkdir checkpoints/Reddit/fine_tuned_test/gin_model
+
